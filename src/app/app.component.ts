26d774017580e8041204c9513c0c328bcfc587da
DominicSylvesterPeters <dominicsylvester95@gmail.com>
2021-01-13 22:56:36 +0530

    After base setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from './service/http.service';
+import { HttpDetailsService } from './service/httpDetails.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'facebook-clone';
+export class AppComponent implements OnInit {
+  constructor(private httpService: HttpService, private httpDetails: HttpDetailsService) { }
+
+  userData;
+  userDetails;
+
+  ngOnInit() {
+    this.httpService.getUsers().subscribe((data) => {
+      console.log(data);
+      this.userData = data;
+    })
+  }
+
+  getUserDetails(selectedUser) {
+    this.httpDetails.getUserDetails(selectedUser).subscribe((details) => {
+      this.userDetails = details;
+    })
+    console.log(this.userDetails)
+  }
+  refresh(){
+    window.location.reload();
+  }
+
 }
